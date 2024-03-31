78274a0f4612f716a26462e2c4092fa8387f2bc0
JEAN\szuje <szujean45@gmail.com>
2024-03-31 23:25:31 +0800

    Modify navbar and sidebar css values

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -15,7 +15,7 @@ export const Sidebar = () => {
             }
         },
         closed:{
-            clipPath: "circle(30px at 50px 50px",
+            clipPath: "circle(30px at 50px 50px)",
             transition:{
                 delay:0.2,
                 type:"spring",
