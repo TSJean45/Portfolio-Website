2ee7721e637becc1063cd34aa692d799bb8fd518
JEAN\szuje <szujean45@gmail.com>
2024-03-31 13:09:04 +0800

    Init homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import "./app.scss";
+import { Navbar } from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+      </section>
+      <section id="About%20Me">Parallax</section>
+      <section>About Me</section>
+      <section id="Experience">Experience</section>
+      <section id="Skills">Skills</section>
+      <section id="Projects">Projects</section>
+      <section id="Achievements">Achievements</section>
+      <section id="Contact">Contact</section>
+    </div>
+  );
 };
 
 export default App;
